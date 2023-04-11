346d4cd03328837ed6cda367a8ed832719e4cba1
Andrey <andriei.shtyrov@mail.ru>
2023-04-11 19:03:51 +0300

    Add WpfComponent to view cube

--- CubeGeneratorWpf/CubeView.xaml.cs
+++ CubeGeneratorWpf/CubeView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CubeGenerator;
 
 namespace CubeGeneratorWpf
 {
@@ -23,6 +24,62 @@ namespace CubeGeneratorWpf
         public CubeView()
         {
             InitializeComponent();
+        }
+
+        public void ChangeView(TableMatrix table)
+        {
+            Field.Children.Clear();
+            var width = Field.ActualWidth;
+            var height = Field.ActualHeight;
+            var nSupportLines = table.Size - 1;
+            var widthStep = width / nSupportLines;
+            var heightStep = height / nSupportLines;
+            List<Line> lines = new List<Line>();
+            for (var i = 0; i < nSupportLines; i++)
+            {
+                var line = new Line();
+                line.X1 = (i + 1) * widthStep;
+                line.X2 = (i + 1) * widthStep;
+                line.Y1 = 0.0;
+                line.Y2 = height;
+                line.Stroke = Brushes.Blue;
+                line.StrokeThickness = 1;
+                lines.Add(line);
+            }
+            for (var i = 0; i < nSupportLines; i++)
+            {
+                var line = new Line();
+                line.X1 = 0.0;
+                line.X2 = width;
+                line.Y1 = (i + 1) * heightStep;
+                line.Y2 = (i + 1) * heightStep;
+                line.Stroke = Brushes.Blue;
+                line.StrokeThickness = 1;
+                lines.Add(line);
+            }
+            List<Polygon> polygons = new List<Polygon>();
+            for (var i = 0; i < table.Size; i++)
+                for (var j = 0; j < table.Size; j++)
+                {
+                    if (!table.Filled[i, j])
+                        continue;
+                    var polygon = new Polygon();
+                    polygon.Fill = Brushes.Black;
+                    PointCollection points = new PointCollection();
+                    points.Add(new Point(i * widthStep, j * heightStep));
+                    points.Add(new Point((i + 1) * widthStep, j * heightStep));
+                    points.Add(new Point((i + 1) * widthStep, (j + 1) * heightStep));
+                    points.Add(new Point(i * widthStep, (j + 1) * heightStep));
+                    polygons.Add(polygon);
+                }
+            foreach (var item in lines)
+            {
+                Field.Children.Add(item);
+            }
+            foreach (var item in polygons)
+            {
+                Field.Children.Add(item);
+            }
         }
     }
 }
