53f160a9c0f0340ed0d73e3949da1b654d9e880a
Andrey <andriei.shtyrov@mail.ru>
2023-04-26 19:04:24 +0300

    Correct Random Fill and naming

--- CubeGenerator/DataController.cs
+++ CubeGenerator/DataController.cs
@@ -45,6 +45,8 @@ namespace CubeGenerator
                 for(int i = 0; i < 4; i++)
                 {
                     var slots = cube.getSlots();
+                    if (slots.Count == 0)
+                        continue;
                     bool notBlocked = true;
                     if (IsCheckMirror)
                     {
@@ -89,22 +91,45 @@ namespace CubeGenerator
             List<List<int>> vs = new();
             for(int i = 1; i < 4; i++)
             {
-                var shift = new List<int>();
-                for (int j = 1; j < seq.Count; j++)
-                    shift.Add((seq[j] / 4) * i + seq[j] % 4);
-                vs.Add(shift);
+                var _seq = new List<int>();
+                for (int j = 0; j < seq.Count; j++)
+                {
+                    if (seq[j] + ( 4 * i) < 16)
+                    {
+                        _seq.Add(seq[j] + (4 * i));
+                    }
+                    else
+                    {
+                        _seq.Add(seq[j] + (4 * i) - 16);
+                    }
+                }
+                vs.Add(_seq);
             }
             return vs;
         }
         public void RandomGeneration(int slotsAmount, int nstructs)
         {
             var seqs = new List<List<int>>();
-            int maxTries = 500;
+            int maxTriesSlots = 500;
+            int i_struct = 0;
+            Int64 maxTriesStructs = nstructs * 2000;
             Random rnd = new Random();
             if (slotsAmount > 12)
                 return;
-            for (int i = 0; i < nstructs; i++)
+            int borderI = 0;
+            if (Borders.Count != 0)
             {
+                var lastIndex = Borders[Borders.Count - 1].Name.Split(' ');
+                int.TryParse(lastIndex[lastIndex.Length - 1], out borderI);
+            }
+            Int64 i = 0;
+            while (true)
+            {
+                if (i > maxTriesStructs)
+                    break;
+                if (i_struct == nstructs)
+                    break;
+                i++;
                 var seq = new List<int>();
                 for (int j = 0; j < slotsAmount; j++)
                 {
@@ -132,31 +157,32 @@ namespace CubeGenerator
                             seq.Add(pos);
                             break;
                         }
-                        if (nstep > maxTries)
+                        if (nstep > maxTriesSlots)
                             break;
                         nstep++;
                     }
                     
                 }
+
                 if (seq.Count == slotsAmount)
                 {
                     var shifted = shiftSeq(seq);
-                    if (!seqs.Contains(seq) && !seqs.Contains(shifted[0]) &&
-                        !seqs.Contains(shifted[1]) && !seqs.Contains(shifted[2]))
-                        seqs.Add(seq);
+                    if (seqs.Contains(seq) || seqs.Contains(shifted[0]) ||
+                        seqs.Contains(shifted[1]) || seqs.Contains(shifted[2]))
+                        continue;
                 }
-            }
-
-            var borderI = 0;
-            foreach (var seq in seqs)
-            {
+                seqs.Add(seq);
                 var border = TableMatrix.GenerateFromSeq(seq, CubeSize, "Border " + borderI.ToString());
-                if (Check(TableMatrix.GenerateFromSeq(seq, CubeSize, "Border")))
+                if (Check(border))
                 {
                     Borders.Add(border);
                     borderI++;
+                    i_struct++;
                 }
+
             }
+
+            
         }
 
         private void UpdateProgramData()
@@ -277,10 +303,28 @@ namespace CubeGenerator
 
         public void CreateNewCube(CubeType cubeType)
         {
+
             if (cubeType is CubeType.Cube)
-                Cubes.Add(new TableMatrix(CubeSize, CubeType.Cube, "Cube " + Cubes.Count));
+            {
+                int borderI = Cubes.Count;
+                if (Cubes.Count != 0)
+                {
+                    var lastIndex = Cubes[Cubes.Count - 1].Name.Split(' ');
+                    int.TryParse(lastIndex[lastIndex.Length - 1], out borderI);
+                }
+                Cubes.Add(new TableMatrix(CubeSize, CubeType.Cube, "Cube " + (borderI + 1)));
+            }
+
             else
-                Borders.Add(new TableMatrix(CubeSize, CubeType.Border, "Border " + Borders.Count));
+            {
+                int borderI = Borders.Count;
+                if (Borders.Count != 0)
+                {
+                    var lastIndex = Borders[Borders.Count - 1].Name.Split(' ');
+                    int.TryParse(lastIndex[lastIndex.Length - 1], out borderI);
+                }
+                Borders.Add(new TableMatrix(CubeSize, CubeType.Border, "Border " + (borderI + 1)));
+            }
         }
 
         public class ProgramSettingSave
